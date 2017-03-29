0e041a45cf737cfd22a0a1a1ba95621ed52805a1
damljanovicdunja <damljanovicdunja96@gmail.com>
2017-03-29 16:44:51 +0200

    Implementirane metode Klase Menjacnica,koja implementira interfejs KursMetode.

--- Menjacnica/src/valuta/Menjacnica.java
+++ Menjacnica/src/valuta/Menjacnica.java
@@ -1,27 +1,49 @@
 package valuta;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import specifikacijainterfejsa.KursMetode;
 
 public class Menjacnica implements KursMetode {
-
+	LinkedList<Valuta> kursnaLista = new LinkedList<Valuta>();
 	@Override
 	public void dodajKurs(GregorianCalendar datum, String naziv, String skraceni, double prodajni, double srednji,
 			double kupovni) {
 		// TODO Auto-generated method stub
+		Valuta nova = new Valuta();
+		nova.setDatumKursa(datum);
+		nova.setNaziv(naziv);
+		nova.setSkraceniNaziv(skraceni);
+		nova.setProdajniKurs(prodajni);
+		nova.setSrednjiKurs(srednji);
+		nova.setKupovniKurs(kupovni);
 		
+		if (kursnaLista.contains(nova)) {
+			throw new RuntimeException("Kurs za ovaj datum vec postoji");
+		}
+		kursnaLista.add(nova);
 	}
 
 	@Override
 	public void obrisiKurs(GregorianCalendar datum, String naziv) {
 		// TODO Auto-generated method stub
-
+		for (int i = 0; i < kursnaLista.size(); i++) {
+			if(kursnaLista.get(i).getDatumKursa() == datum && kursnaLista.get(i).getNaziv().equals(naziv)){
+				kursnaLista.remove(i);
+			}
+		}
 	}
 
 	@Override
 	public Valuta pronadjiKurs(GregorianCalendar datum, String naziv) {
 		// TODO Auto-generated method stub
+		for (int i = 0; i < kursnaLista.size(); i++) {
+			if(kursnaLista.get(i).getDatumKursa() == datum && kursnaLista.get(i).getNaziv().equals(naziv)){
+				return kursnaLista.get(i);
+			}else throw new RuntimeException("Ne postoji trazeni kurs");
+		}
+		
 		return null;
 	}
 

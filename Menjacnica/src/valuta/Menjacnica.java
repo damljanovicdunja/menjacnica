924d84600941e0c208b9a8223bbae634c6dbf0cc
damljanovicdunja <damljanovicdunja96@gmail.com>
2017-03-29 16:55:01 +0200

    Implementirana metoda dodajKurs u okviru klase Menjacnica.

--- Menjacnica/src/valuta/Menjacnica.java
+++ Menjacnica/src/valuta/Menjacnica.java
@@ -1,16 +1,31 @@
 package valuta;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import specifikacijainterfejsa.KursMetode;
 
 public class Menjacnica implements KursMetode {
-
+	LinkedList<Valuta> kursnaLista = new LinkedList<Valuta>();
 	@Override
 	public void dodajKurs(GregorianCalendar datum, String naziv, String skraceni, double prodajni, double srednji,
 			double kupovni) {
 		// TODO Auto-generated method stub
+		Valuta nova = new Valuta();
+		nova.setDatumKursa(datum);
+		nova.setNaziv(naziv);
+		nova.setSkraceniNaziv(skraceni);
+		nova.setKupovniKurs(kupovni);
+		nova.setProdajniKurs(prodajni);
+		nova.setSrednjiKurs(srednji);
 		
+		for (int i = 0; i < kursnaLista.size(); i++) {
+			Valuta v = kursnaLista.get(i);
+			if(v.getDatumKursa() ==datum && v.getNaziv().equals(naziv))
+				throw new RuntimeException("Vec postoji kurs za ovaj datum.");
+			else kursnaLista.add(v);
+			
+		}
 	}
 
 	@Override
